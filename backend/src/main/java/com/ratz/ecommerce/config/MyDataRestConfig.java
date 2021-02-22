631c508b3f9b3c640b6ccc526c67e6ce255b4957
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-22 17:45:04 -0100

    added configuration class for make our RestAPI READ ONLY ( for now) , Post,put,patch delete methods disabled

---
+++ backend/src/main/java/com/ratz/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,32 @@
+package com.ratz.ecommerce.config;
+
+
+import com.ratz.ecommerce.entity.Product;
+import com.ratz.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.PUT, HttpMethod.DELETE};
+
+        //disable HTTP methods for product: put, patch delete
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+
+        //disable HTTP methods for product_category : put, patch delete
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
