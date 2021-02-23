1d88e07a7149480a11c190f2a19c69f15c66a447
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-23 14:34:07 -0100

    Added CORS support in the backend, Created Product class and ProductService.  Created component that uses the ProductList Service to display our Products in the main page. Full-stack integration completed

--- backend/src/main/java/com/ratz/ecommerce/entity/ProductCategory.java
+++ backend/src/main/java/com/ratz/ecommerce/entity/ProductCategory.java
@@ -10,8 +10,6 @@ import java.util.Set;
 
 @Entity
 @Table(name="product_category")
-@Getter
-@Setter
 public class ProductCategory {
 
     @Id
@@ -24,4 +22,28 @@ public class ProductCategory {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
 }
