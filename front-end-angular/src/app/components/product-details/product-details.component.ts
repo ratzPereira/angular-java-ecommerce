de4d6cfd98cddad65a91043fab3dcf1168aa4412
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-28 15:40:20 -0100

    fixed bug. we had race condition when we tried to access the imageUrl, instantiated new Product in  our product-details.ts so we can have access to imageUrl propertie

--- front-end-angular/src/app/components/product-details/product-details.component.ts
+++ front-end-angular/src/app/components/product-details/product-details.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from '../../services/product.service';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-  product: Product;
+  product: Product = new Product();
 
   constructor(
     private productService: ProductService,
