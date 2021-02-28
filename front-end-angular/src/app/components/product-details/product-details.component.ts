a6f3b8b48ef578b47b4fff8903ce8b4d72d4998b
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-28 15:29:07 -0100

    Implemented product details. now we can click in the product img or name and see the product details. also added button to "back to products"

---
+++ front-end-angular/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
+  }
+  handleProductDetails() {
+    //get the id param string. convert to an umber using the "+" symbol
+
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(theProductId).subscribe((data) => {
+      this.product = data;
+    });
+  }
+}
