e8d6be336da43ed885aba2f360ea80d3d3de01fc
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-28 14:24:38 -0100

    fully implemented functionality in our search bar and refactored the code to be more clean.  need to add the feature for "handle for no products found"

--- front-end-angular/src/app/services/product.service.ts
+++ front-end-angular/src/app/services/product.service.ts
@@ -19,9 +19,20 @@ export class ProductService {
     //need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
+    return this.getProducts(searchUrl); // the JSON from our api send the data like this, _embedded.products
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    //need to build URL based on keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient
       .get<GetResponseProducts>(searchUrl)
-      .pipe(map((response) => response._embedded.products)); // the JSON from our api send the data like this, _embedded.products
+      .pipe(map((response) => response._embedded.products));
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
